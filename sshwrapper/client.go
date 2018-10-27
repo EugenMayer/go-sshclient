74e6f27b0200047acefe73fbaef649745d3f595c
em <eugen.mayer@kontextwork.de>
2018-10-27 10:24:44 +0200

    add convinient RUN command to quick run a command, add docs

--- sshwrapper/client.go
+++ sshwrapper/client.go
@@ -8,6 +8,7 @@ import (
 	"time"
 )
 
+// connect the ssh client and create a session, ready to go with commands ro scp
 func (sshApi *SshApi) ConnectAndSession() (err error) {
 	if client, err := sshApi.Connect(); err != nil {
 		return err
@@ -18,6 +19,7 @@ func (sshApi *SshApi) ConnectAndSession() (err error) {
 	return sshApi.SessionDefault()
 }
 
+// creates a default session with usual parameters
 func (sshApi *SshApi) SessionDefault() (err error) {
 	if session, err := sshApi.Client.NewSession(); err != nil {
 		return err
@@ -41,6 +43,9 @@ func (sshApi *SshApi) SessionDefault() (err error) {
 	return nil
 }
 
+// connect the ssh client - use ConnectAndSession if you have no reason to create
+// the session manually why ever
+// we do support proper timeouts here, thats why it looks a little more complicated then the usual ssh connect
 func (sshApi *SshApi) Connect() (*ssh.Client, error) {
 	var addr = fmt.Sprintf("%s:%d", sshApi.Host, sshApi.Port)
 	conn, err := net.DialTimeout("tcp", addr, sshApi.SshConfig.Timeout)
@@ -58,14 +63,31 @@ func (sshApi *SshApi) Connect() (*ssh.Client, error) {
 	return ssh.NewClient(c, chans, reqs), err
 }
 
+// get the stdout from your last command
 func (sshApi *SshApi) GetStdOut() string {
 	return sshApi.StdOut.String()
 }
 
+// get the stderr from your last command
 func (sshApi *SshApi) GetStdErr() string {
 	return sshApi.StdErr.String()
 }
 
+// run a ssh command. Auto-creates session if you did yet not connect
+// just wrapping ssh.Session.Run with connect / run and then disconnect
+func (sshApi *SshApi) Run(cmd string) (stdout string, stderr string, err error) {
+	if sshApi.Session == nil {
+		if err = sshApi.ConnectAndSession(); err != nil {
+			return "","", err
+		}
+	}
+
+	err = sshApi.Session.Run(cmd)
+	sshApi.Session.Close()
+	return  sshApi.GetStdOut(),sshApi.GetStdErr(), err
+}
+
+// scp a local file to a remote host
 func (sshApi *SshApi) CopyToRemote(source string, dest string) (err error) {
 	sshApi.ConnectAndSession()
 	err = scpwrapper.CopyToRemote(source, dest, sshApi.Session)
@@ -73,6 +95,7 @@ func (sshApi *SshApi) CopyToRemote(source string, dest string) (err error) {
 	return err
 }
 
+// scp a file from a remote host
 func (sshApi *SshApi) CopyFromRemote(source string, dest string) (err error) {
 	sshApi.ConnectAndSession()
 	err = scpwrapper.CopyFromRemote(source, dest, sshApi.Session)
